9e3d71f774cd18ef7abb2473a2ab675fdeabbb37
mbae26 <minseok@stanford.edu>
2023-07-14 10:48:39 -0500

    Edited utils.py to remove References part

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+import re
 import pdftotext
 
 def convert_pdf_to_txt(directory):
@@ -12,6 +13,15 @@ def convert_pdf_to_txt(directory):
                 with open(pdf_path, "rb") as f:
                     pdf = pdftotext.PDF(f)
                 text = "\n\n".join(pdf)
+                
+                # Remove References or Bibliography parts of the texts
+                # Make sure to check if the word, 'References' or 'Bibliography'
+                # appears after at least 60% of the texts. 
+                if 'References' in text[int(0.6*len(text)):]:
+                    text = text[:text.rfind('References')]
+                elif 'Bibliography' in text[int(0.6*len(text)):]:
+                    text = text[:text.rfind('Bibliography')]
+                    
                 with open(txt_path, "w") as text_file:
                     text_file.write(text)
 
