2e0cc6f5d0d77e121f0175b5da371ae496b50439
mbae26 <minseok@stanford.edu>
2023-07-14 00:15:22 -0500

    implemented utils.py for text extraction from PDFs

---
+++ utils.py
@@ -0,0 +1,19 @@
+import os
+import pdftotext
+
+def convert_pdf_to_txt(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith(".pdf"):
+            txt_filename = f"{os.path.splitext(filename)[0]}.txt"
+            txt_path = os.path.join(directory, txt_filename)
+            # Check if the txt file already exists
+            if not os.path.exists(txt_path):
+                pdf_path = os.path.join(directory, filename)
+                with open(pdf_path, "rb") as f:
+                    pdf = pdftotext.PDF(f)
+                text = "\n\n".join(pdf)
+                with open(txt_path, "w") as text_file:
+                    text_file.write(text)
+
+dir_path = "/Users/minseokbae/ANL/gpt3_finetune/pdfs"
+convert_pdf_to_txt(dir_path)
