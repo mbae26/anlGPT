bf9192d77d5fa1c9092b6ee6bd774b2756f7f0ce
mbae26 <minseok@stanford.edu>
2023-07-14 19:04:19 -0500

    Edited utils.py for more efficient removal and extracted texts from the PDFs

--- utils.py
+++ utils.py
@@ -16,12 +16,31 @@ def convert_pdf_to_txt(directory):
                 
                 # Remove References or Bibliography parts of the texts
                 # Make sure to check if the word, 'References' or 'Bibliography'
-                # appears after at least 60% of the texts. 
-                if 'References' in text[int(0.6*len(text)):]:
-                    text = text[:text.rfind('References')]
-                elif 'Bibliography' in text[int(0.6*len(text)):]:
-                    text = text[:text.rfind('Bibliography')]
-                    
+                # appears after at least 70% of the texts. 
+                text_lower = text.lower()
+
+                # Extract the last 30% of the text
+                last_30_percent = text_lower[int(0.7*len(text)):]
+
+                # Find the index of 'references' and 'acknowledgements' within the last 30% of the text
+                references_index = last_30_percent.find('references')
+                acknowledgements_index = last_30_percent.find('acknowledgements')
+
+                # Initialize the index at which to start removing text as -1 (not found)
+                remove_text_index = -1
+
+                # If 'references' is found within the last 30% of the text, update remove_text_index
+                if references_index != -1:
+                    remove_text_index = references_index + int(0.7*len(text))
+
+                # If 'acknowledgements' is found within the last 30% of the text and appears before 'references', update remove_text_index
+                if acknowledgements_index != -1 and (acknowledgements_index < references_index or references_index == -1):
+                    remove_text_index = acknowledgements_index + int(0.7*len(text))
+
+                # If either term was found within the last 30% of the text, remove everything from that point onwards
+                if remove_text_index != -1:
+                    text = text[:remove_text_index]
+
                 with open(txt_path, "w") as text_file:
                     text_file.write(text)
 
