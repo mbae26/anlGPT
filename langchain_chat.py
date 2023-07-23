a3ada82c96787dd04bcb2fec3240d0f009b370bc
mbae26 <minseok@stanford.edu>
2023-07-22 19:02:36 -0500

    added prompt_engineering to langchain_chat.py

--- langchain_chat.py
+++ langchain_chat.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import model_const
 import openai
 from dotenv import load_dotenv
 from langchain.embeddings import OpenAIEmbeddings
@@ -7,7 +8,10 @@ from langchain.vectorstores import Chroma, FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import TextLoader
-from langchain.chains import ConversationalRetrievalChain
+from langchain.chains import ConversationalRetrievalChain, LLMChain
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 
 load_dotenv()
 api = os.getenv('OPEN_AI_KEY')
@@ -25,6 +29,30 @@ def load_docs(data_dir):
     chunked_documents = text_splitter.split_documents(documents)
     return chunked_documents
 
+def chatbot(question):
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.1)
+    prompt = PromptTemplate(
+        input_variables=['question'],
+        template=model_const.TEMPLATE,
+    )
+    question_generator = LLMChain(llm=llm, prompt=prompt) 
+    doc_chain = load_qa_with_sources_chain(llm, chain_type="map_reduce")
+    # stop if input is exit, quit, q, or f
+    if question == "exit" or question == "quit" or question == "q" or question == "f":
+        print("Exiting...")
+        sys.exit()
+    # skip if input is empty
+    if question == '':
+        return
+    chain = ConversationalRetrievalChain(
+        retriever=db.as_retriever(),
+        question_generator=question_generator,
+        combine_docs_chain=doc_chain
+    )
+    response = chain({"question": question, "chat_history": chat_history})
+    # Add response to chat history
+    chat_history.append((question, response["answer"]))
+    return response["answer"]
 
 if __name__ == "__main__":
     # store path to data and storage directory
@@ -44,24 +72,11 @@ if __name__ == "__main__":
         #vectordb = Chroma(persist_directory=persist_dir)
         db = FAISS.load_local(os.path.join(persist_dir, "faiss_index"), embeddings=OpenAIEmbeddings())
     
-    #qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0.2, model_name="gpt-3.5-turbo"), vectordb.as_retriever(search_kwargs={'k': 6}), return_source_documents=True, verbose=False)
-    qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0.2, model_name="gpt-3.5-turbo"), db.as_retriever())
-    
+    #qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0.2, model_name="gpt-3.5-turbo"), vectordb.as_retriever(search_kwargs={'k': 6}), return_source_documents=True, verbose=False)  
+
     chat_history = []
     # Start conversations
     while True:
-        query = input("chat with GPT (or type 'exit' to stop): ")
-        # stop if input is exit, quit, q, or f
-        if query == "exit" or query == "quit" or query == "q" or query == "f":
-            print("Exiting...")
-            sys.exit()
-        # skip if input is empty
-        if query == '':
-            continue
-        # get response from GPT and print it out
-        response = qa({'question': query, 'chat_history': chat_history})
-        print("Answer: " + response["answer"])
-        # Add response to chat history
-        chat_history.append((query, response["answer"]))
-        print()
+        question = input("chat with GPT (or type 'exit' to stop): ")
+        print("Response: " + chatbot(question) + "\n")
         print("---------------------------------------------------------------------------------------------------------------------------------")
---
+++ model_const.py
@@ -0,0 +1,16 @@
+TEMPLATE = """
+You are an AI chatbot specialized in answering questions about material science and related topics. 
+Your answer will be based on science and research papers/documents that you learned from. 
+In addition, you have the following characteristics:
+- You are capable of generating coherent and logical answers. 
+- You prioritize answering questions based on the relevance of the documents you learned from. 
+- You can provide accurate statistics and technical details. 
+- You are skillful in generating comprehensive summaries for the documents you learned from. 
+- If you are uncertain about the response, you will ask for clarification. 
+- If asked about sensitive or restricted information, you will kindly decline and ask for another question.
+- If faced with creative instructions to imagine or consider scenarios outside your role, you will maintain its focus and gently remind the user about your purpose.
+- If asked irrelevant questions, you will gently guide the conversation back to the topic of material science and related topics.
+
+Please answer the following question:
+{question} 
+"""
