69bf0d8ee20d6092fa386d87e2ae0c15a0020ce7
mbae26 <minseok@stanford.edu>
2023-08-01 10:14:20 -0500

    Refined langchain_grobid.py.

--- langchain_grobid.py
+++ langchain_grobid.py
@@ -1,23 +1,27 @@
 import os
-import sys
 import constants
 import openai
-from dotenv import load_dotenv
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import UnstructuredXMLLoader
-from langchain.chains import ConversationalRetrievalChain, LLMChain
+from langchain.chains import ConversationalRetrievalChain, LLMChain, RetrievalQA
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains.qa_with_sources import load_qa_with_sources_chain
-from langchain.retrievers.document_compressors import EmbeddingsFilter
 
-load_dotenv()
-api = os.getenv('OPEN_AI_KEY')
-os.environ["OPENAI_API_KEY"] = api
-llm = ChatOpenAI(model_name="gpt-4", temperature=0)
+os.environ["OPENAI_API_KEY"] = constants.OPENAI_API
+data_dir = "./data/grobid_files"
+persist_dir = "./faiss_grobid"
+
+def set_qa_prompt():
+    """
+    Define prompt template for QA
+    """
+    prompt = PromptTemplate(
+        template=constants.QA_TEMPLATE,
+        input_variables=["context", "question"],
+    )
+    return prompt
 
 def load_docs(data_dir):
     # Load the documents
@@ -26,58 +30,53 @@ def load_docs(data_dir):
         if file.endswith(".xml"):
             loader = UnstructuredXMLLoader(os.path.join(data_dir, file))
             documents.extend(loader.load())
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     chunked_documents = text_splitter.split_documents(documents)
     return chunked_documents
 
-def chatbot(question):
-    prompt = PromptTemplate(
-        input_variables=['question'],
-        template=constants.TEMPLATE,
-    )
-    
-    # embeddings_filter = EmbeddingsFilter(embeddings=embeddings, similarity_threshold=0.76)
-    # compression_retriever = ContextualCompressionRetriever(base_compressor=embeddings_filter, base_retriever=retriever)
-
-    # compressed_docs = compression_retriever.get_relevant_documents("What did the president say about Ketanji Jackson Brown")
-    # pretty_print_docs(compressed_docs)
-    
-    question_generator = LLMChain(llm=llm, prompt=prompt) 
-    doc_chain = load_qa_with_sources_chain(llm, chain_type="stuff")
-    # stop if input is exit, quit, q, or f
-    if question == "exit" or question == "quit" or question == "q" or question == "f":
-        print("Exiting...")
-        sys.exit()
-    # skip if input is empty
-    if question == '':
-        return
-    chain = ConversationalRetrievalChain(
-        retriever=db.as_retriever(search_kwargs={'k': 4}),
-        question_generator=question_generator,
-        combine_docs_chain=doc_chain,
-    )
-    response = chain({"question": question, "chat_history": chat_history})
-    # Add response to chat history
-    chat_history.append((question, response["answer"]))
-    return response["answer"]
-
-if __name__ == "__main__":
-    # store path to data and storage directory
-    data_dir = "./data/grobid_files"
-    persist_dir = "./faiss_grobid"
-    
+def main():
+    # If vector storage exists, load it. Otherwise, create one. 
     if not os.path.exists(persist_dir):
         chunked_documents = load_docs(data_dir)
         db = FAISS.from_documents(chunked_documents, embedding=OpenAIEmbeddings())
         os.makedirs(persist_dir)  # Create persist_dir before saving
         db.save_local(os.path.join(persist_dir, "faiss_index"))  # Save faiss_index inside persist_dir
-
     else:
-        db = FAISS.load_local(os.path.join(persist_dir, "faiss_index"), embeddings=OpenAIEmbeddings())
-
-    chat_history = []
-    # Start conversations
+        db = FAISS.load_local(os.path.join(persist_dir, "faiss_index"), embeddings=OpenAIEmbeddings())    
+    
+    # Load prompt template
+    qa_prompt = set_qa_prompt()
+    
+    # Initialize retriever 
+    retriever = db.as_retriever(search_kwargs={'k': 4})
+    
+    # Initialize QA chain
+    qa = RetrievalQA.from_chain_type(
+        llm=constants.llm,
+        chain_type="stuff",
+        retriever=retriever,
+        return_source_documents=True,
+        chain_type_kwargs={'prompt': qa_prompt},
+    ) 
+    
+    # Interactive questions and answers
     while True:
-        question = input("chat with GPT (or type 'exit' to stop): ")
-        print("Response: " + chatbot(question) + "\n")
-        print("---------------------------------------------------------------------------------------------------------------------------------")
+        query = input("\nAsk a question: ")
+        if query == "exit" or query == "quit" or query == "q":
+            break
+        # Get the answer from the QA chain
+        response = qa(query)
+        answer, source_documents = response["result"], response["source_documents"]
+        
+        # Print the answer
+        print("\n-> Answer:")
+        print(answer)
+        
+        # Print the source documents if 'show_sources' is True
+        for document in source_documents:
+            print("\n> " + document.metadata["source"])
+        print("----------------------------------SOURCE DOCUMENTS---------------------------")
+
+
+if __name__ == "__main__":
+    main()
